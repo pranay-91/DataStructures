463fc84af61bb90b879703eb955c2fa68c45a6f5
Pranay Pradhananga <pronoy19114@gmail.com>
2017-09-15 17:18:30 +1000

    Changed to fixed array Stack implementation and Added returnTheMaximum

--- src/challenges/StackWithMax.java
+++ src/challenges/StackWithMax.java
@@ -1,36 +1,26 @@
 /*
-Stack implementation in Java using Resizing Array Data Structure
+Stack implementation in Java using Array Data Structure
     
     items[] |to|be|or|not|to|be|null|null|
              0  1  2  3   4  5   6   7   
-                      ^              
-                    index           
+                      ^              ^
+                    index           size
                    push/pop
-                   
-Action: Push
-Condition: 
-    if stack is full
-        increase size by 2
-    
-Action: Pop
-Condition: 
-    if stack is 25% full
-        decrease the array size by 2
 
-Invariant: Array is between 25% and 100% full.
-
-Amortorized Analysis:
-- Average running time per operation over a worst-case sequence of operations.
-- Every operation takes constant amortized time.
-- Less wasted space.
+Analysis:
+- Every operation takes constant time.
+- Stack Array is fixed and cannot be increased during run time.
 */
 
 package challenges;
 
-public class StackWithMax<Item> {
+public class StackWithMax {
     private int index = 0;
-    private static int DEFAULT_SIZE = 10;
-    private Item[] items = (Item[])new Object[DEFAULT_SIZE]; // Typecasting. Item extends Object Class.
+    private double[] items;
+
+    public StackWithMax(int size) {
+        items = new double[size];
+    }
 
     public boolean isEmpty() {
         return index == 0;
@@ -40,39 +30,32 @@ public class StackWithMax<Item> {
         return index == items.length;
     }
 
-    // Helper function to resize an array
-    private void resize(int size) {
-        // Typecasting. Item extends Object Class. 
-        // Need to investigate what are the possible drawbacks for typecasting during runtime.
-        Item[] copy = (Item[])new Object[size]; 
-        for(int i=0; i<index; ++i) {
-            copy[i] = items[i];
-        }
-        items = copy;
-    }
-
-    public void push(Item s){
+    public void push(double s){
         if(isFull()) {
-            // If full then increase the stack array by twice the current size
-            // System.out.println("\n\tIncreasing stack array size.");
-            resize(items.length * 2);
+            throw new RuntimeException("Stack is Full!");
         }
+        //use to index the array and then increase the index
         items[index++] = s;
     }
 
-    public Item pop() {
+    public double pop() {
         if(isEmpty()) {
             throw new RuntimeException("Stack is Empty!");
         }
-
-        Item s = items[--index];
-        items[index] = null;
-        
-        if(index>0 && index == items.length/4) {
-            // If only 25% is being used then shrink the array by half.
-            // System.out.println("\n\tShrinking stack array size.");
-            resize(items.length/2);
-        }
-        return s;
+        // reduce index and then access the array
+        return items[--index];
     }
+
+    public double returnTheMaximum() {
+        double max = items[index];
+
+        for(int i=0; i<=index; ++i) {
+            if(max<items[i]) {
+                max = items[i];
+            }
+        }
+        return max;
+    }
+    
+
 }
