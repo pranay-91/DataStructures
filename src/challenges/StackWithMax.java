5d2cc546d3a464c35d7e3ec72ddc051c80947695
Pranay Pradhananga <pronoy19114@gmail.com>
2017-09-15 16:06:22 +1000

    Added challenges folder and StackWithMax by reusing StackResizingArray

---
+++ src/challenges/StackWithMax.java
@@ -0,0 +1,76 @@
+/*
+Stack implementation in Java using Resizing Array Data Structure
+    
+    items[] |to|be|or|not|to|be|null|null|
+             0  1  2  3   4  5   6   7   
+                      ^              
+                    index           
+                   push/pop
+                   
+Action: Push
+Condition: 
+    if stack is full
+        increase size by 2
+    
+Action: Pop
+Condition: 
+    if stack is 25% full
+        decrease the array size by 2
+
+Invariant: Array is between 25% and 100% full.
+
+Amortorized Analysis:
+- Average running time per operation over a worst-case sequence of operations.
+- Every operation takes constant amortized time.
+- Less wasted space.
+*/
+
+package datastructures;
+
+public class StackResizingArray<Item> {
+    private int index = 0;
+    private static int DEFAULT_SIZE = 10;
+    private Item[] items = new String[DEFAULT_SIZE];
+
+    public boolean isEmpty() {
+        return index == 0;
+    }
+
+    public boolean isFull() {
+        return index == items.length;
+    }
+
+    // Helper function to resize an array
+    private void resize(int size) {
+        String[] copy = new Item[size];
+        for(int i=0; i<index; ++i) {
+            copy[i] = items[i];
+        }
+        items = copy;
+    }
+
+    public void push(Item s){
+        if(isFull()) {
+            // If full then increase the stack array by twice the current size
+            System.out.println("\n\tIncreasing stack array size.");
+            resize(items.length * 2);
+        }
+        items[index++] = s;
+    }
+
+    public Item pop() {
+        if(isEmpty()) {
+            throw new RuntimeException("Stack is Empty!");
+        }
+
+        Item s = items[--index];
+        items[index] = null;
+        
+        if(index>0 && index == items.length/4) {
+            // If only 25% is being used then shrink the array by half.
+            System.out.println("\n\tShrinking stack array size.");
+            resize(items.length/2);
+        }
+        return s;
+    }
+}
